eea5a67bf5270a514bf7e713c9b4ebaf7458cea7
james-e-crandall <james.e.crandall@gmail.com>
2025-12-10 12:50:49 -0600

    Add Keycloak to apphost

--- FamilyWebsite.AppHost/AppHost.cs
+++ FamilyWebsite.AppHost/AppHost.cs
@@ -1,3 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+// Add Keycloak authentication
+var keycloak = builder.AddKeycloak("keycloak")
+    .WithDataVolume();
+
 builder.Build().Run();
